41ac3bd7a98b69f7b93b6ccd554460e030aac8c3
sleepingvoice <rkwhr99@gmail.com>
2020-12-29 14:13:07 +0900

    no message

---
+++ Classes/Boss/m_Boss.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <cocos2d.h>
+USING_NS_CC;
+class m_Boss :public Node
+{
+public:
+	bool init();
+	CREATE_FUNC(m_Boss);
+
+	int Hp;
+	float Time = 0;
+	float shotTime = -1;
+	Vec2 dir;
+	Vec2 target;
+	bool Next;
+	bool Die;
+	bool particle;
+	float DeadTime = 0;
+
+
+	Rect GetBox();
+	void Damage();
+	void update(float dt);
+	void change();
+	void pattern1();
+	void pattern2();
+	void dirMax();
+};
+
