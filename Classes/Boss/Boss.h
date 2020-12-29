70e5f8cdeb35b1faa9490d331748b5181713ea6d
sleepingvoice <rkwhr99@gmail.com>
2020-12-29 16:56:08 +0900

    보스변경

--- Classes/Boss/Boss.h
+++ Classes/Boss/Boss.h
@@ -1,11 +1,11 @@
 #pragma once
 #include <cocos2d.h>
 USING_NS_CC;
-class m_Boss :public Node
+class Boss :public Node
 {
 public:
 	bool init();
-	CREATE_FUNC(m_Boss);
+	CREATE_FUNC(Boss);
 
 	int Hp;
 	float Time = 0;
